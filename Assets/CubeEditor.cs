e31f2804d6b6438bee39d7b677d2b68c5052e8f4
joeface71 <joefacenda@yahoo.com>
2019-06-04 15:29:27 -0400

    First Commit

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] // script executes while editing
+[SelectionBase] // default selection in editor is cube--prevents accidentally selecting something else
+
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+    
+    private void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; // enable snapping to grid
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;        
+    }
+}
