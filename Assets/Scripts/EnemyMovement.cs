a9aef619f9afacfe34d8bcd422b4d1ffa6226817
joeface71 <joefacenda@yahoo.com>
2019-06-11 14:41:05 -0400

    Spawning Coroutine

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -15,12 +15,10 @@ public class EnemyMovement : MonoBehaviour
 
     IEnumerator FollowPath(List<Waypoint> path)
     {
-        print("Starting patrol");
         foreach (Waypoint waypoint in path)
         {
             transform.position = waypoint.transform.position;
             yield return new WaitForSeconds(enemyMovementSpeed);
         }
-        print("Ending patrol");
     }
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab; //Restricts to prefabs with EnemyMovement script
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true) // runs forever
+        {
+            print("Spawning Enemy");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+       
+    }
+}
